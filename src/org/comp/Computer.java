72ee5434f447ac0b5506097cb3c86e87ab9e9497
JayabarathiVeerappan <jayabarathi3797@gmail.com>
2024-08-06 18:01:39 +0530

    push2

--- src/org/comp/Computer.java
+++ src/org/comp/Computer.java
@@ -4,5 +4,8 @@ public class Computer {
 public void computerModel() {
 System.out.println("computer model os is windows10");
 }
+private void computerSize() {
+System.out.println("Computer size is Medium");
 
+}
 }
--- src/org/comp/Device.java
+++ src/org/comp/Device.java
@@ -1,12 +1,13 @@
 package org.comp;
 
-public class Desktop extends Computer {
+public class Device extends Computer {
 private void desktopSize() {
 System.out.println("desktop size is 32 inch");
 }
 public static void main(String[] args) {
-	Desktop d=new Desktop();
+	Device d=new Device();
 	d.desktopSize();
 	d.computerModel();
+	
 }
 }
